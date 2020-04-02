ccfb3f6dd67c9c15697500d1df3e74384a4dea94
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-04-02 11:57:27 +0200

    Fixed statistics csv

--- code/weighted_coauthors_networks_analysis.py
+++ code/weighted_coauthors_networks_analysis.py
@@ -8,7 +8,7 @@ from collections import Counter
 
 path_to_nets = '../datasets/coauthors_networks/network_weight/'
 
-g, years = None, np.arange(1980, 1990)
+g, years = None, np.arange(1990, 2000)
 
 nodes, edges, densities, avg_cc, transitivities, eccen, diameters, rads = \
     list(), list(), list(), list(), list(), list(), list(), list()
