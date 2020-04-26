baad8ee343c3c4aee06c61b4c2fb5f36ef87c9a0
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-04-26 13:13:17 +0200

    Updated zenodo parser script

--- code/zenodo/zenodo_parser.py
+++ code/zenodo/zenodo_parser.py
@@ -8,16 +8,24 @@ from tqdm import tqdm
 
 assert path.exists(sys.argv[1])
 
+not_parsed = 0
+
 with gzip.open(sys.argv[1], 'r') as compressed_file:
     with open(sys.argv[2], 'w') as xml_file:
         for line in tqdm(compressed_file, desc='PARSING TO XML'):
             c_rec = json.loads(line)['body']['$binary']
 
-            record = subprocess.\
-                run('echo {} | base64 --decode | bsdtar -x -O'.format(c_rec),
-                    shell=True, capture_output=True, text=True).stdout
-            record = record.replace('\n', '').replace('\t', '')
-            record = record.replace('<?xml version="1.0" encoding="UTF-8"?>',
-                                    '')
+            try:
+                record = subprocess.\
+                    run('echo {} | base64 --decode | bsdtar -x -O'
+                        .format(c_rec), shell=True, capture_output=True,
+                        text=True).stdout
+                record = record.replace('\n', '').replace('\t', '')
+                record = record\
+                    .replace('<?xml version="1.0" encoding="UTF-8"?>', '')
 
-            xml_file.write(record + '\n')
+                xml_file.write(record + '\n')
+            except Exception as e:
+                not_parsed += 1
+
+print('NOT PARSED: {}'.format(not_parsed))
