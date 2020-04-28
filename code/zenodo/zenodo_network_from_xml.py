b7a887720f09b1d2cf9523126900986139c37a36
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-04-28 18:03:14 +0200

    Updated zenodo network building script

--- code/zenodo/zenodo_network_from_xml.py
+++ code/zenodo/zenodo_network_from_xml.py
@@ -1,13 +1,14 @@
 import json
 import numpy as np
+import os
 import sys
 
 from lxml import etree
+from scipy.stats import mode
 from tqdm import tqdm
 
-creators_infos = dict()
-adjacency_d = dict()
-projects_table = dict()
+node_dict, adjacency_d = dict(), dict()
+framework = sys.argv[1].split('/')[-1].split('_')[1]
 
 with open(sys.argv[1], 'r') as xml_file:
     for record in tqdm(xml_file, desc='BUILDING ADJANCENCY MATRIX'):
@@ -16,120 +17,105 @@ with open(sys.argv[1], 'r') as xml_file:
         creators = r.findall('.//creator')
         creators = [int(dict(creator.attrib)['MAGIdentifier']) for creator in
                     creators if 'MAGIdentifier' in dict(creator.attrib)]
-        affiliation = r.find(".//rel[@inferenceprovenance = "
-                             "'iis::document_affiliations']")
-        project_title = r.find('.//title').text
-        d_of_acc = r.find('.//dateofacceptance').text
+        title = r.find('.//title').text.lower() if r.find('.//title').text is \
+            not None else None
+        year = r.find('.//dateofacceptance').text.split('-')[0] if \
+            r.find('.//dateofacceptance').text is not None else None
+        affiliations = r.findall(".//rel[@inferenceprovenance = "
+                                 "'iis::document_affiliations']")
+        affiliations_list = list()
+        affiliation = None
 
-        if affiliation is not None:
-            if len(creators) != 0 and project_title is not None and \
-                    d_of_acc is not None and \
-                    affiliation.find('country') is not None:
-                country = affiliation.find('country').\
-                    attrib['classname'].lower()
-                d_of_acc = d_of_acc.split('-')[0]
-                project_title = project_title.lower()
+        if len(affiliations) != 0:
+            for aff in affiliations:
+                if aff.find('country') is not None:
+                    affiliations_list.append(aff.find('country').
+                                             attrib['classname'].lower())
 
-# STORING INFOS FOR THE NETWORK'S NODES #######################################
+            if len(affiliations_list) != 0:
+                affiliation = str(mode(affiliations_list)[0][0])
 
-                for creator in creators:
-                    if creator not in creators_infos:
-                        creators_infos[creator] = {d_of_acc: country}
+        context = r.find(".//context[@type = 'community']")
+        project_id, acronym, label = None, None, None
+
+        if context is not None:
+            project_ids = list()
+
+            for concept in context.findall('.//concept'):
+                project_ids.append(concept.attrib['id'])
+
+            if len(project_ids) != 0:
+                project_id = max(project_ids, key=len)
+                label = context.find(".//concept[@id = '{}']"
+                                     .format(project_id))
+                label = label.attrib['label']
+                acronym = r.find('.//acronym').text if r.find('.//acronym') \
+                    is not None else None
+
+        if len(creators) != 0 and affiliation is not None and year is not None:
+            for creator in creators:
+                if year not in node_dict:
+                    node_dict[year] = {creator: affiliation}
+                elif creator not in node_dict[year]:
+                    node_dict[year][creator] = affiliation
+                else:
+                    if type(node_dict[year][creator]) == str:
+                        c = node_dict[year][creator]
+                        node_dict[year][creator] = [c, affiliation]
                     else:
-                        if d_of_acc not in creators_infos[creator]:
-                            creators_infos[creator][d_of_acc] = country
+                        node_dict[year][creator].append(affiliation)
+
+            infos = [title] if project_id is None else [title, project_id]
+
+            for idx in np.arange(len(creators) - 1):
+                cowork = [creators[i] for i in np.arange(idx + 1,
+                                                         len(creators))]
+                for c in cowork:
+                    c1, c2 = None, None
+
+                    if creators[idx] < c:
+                        c1, c2 = creators[idx], c
+                    else:
+                        c1, c2 = c, creators[idx]
+
+                    if year not in adjacency_d:
+                        adjacency_d[year] = {c1: {c2: [infos]}}
+                    elif c1 not in adjacency_d[year]:
+                        adjacency_d[year][c1] = {c2: [infos]}
+                    else:
+                        if c2 not in adjacency_d[year][c1]:
+                            adjacency_d[year][c1][c2] = [infos]
                         else:
-                            c = creators_infos[creator][d_of_acc]
+                            adjacency_d[year][c1][c2].append(infos)
 
-                            if type(c) == str:
-                                if c != country:
-                                    creators_infos[creator][d_of_acc] = \
-                                        [c, country]
-                            else:
-                                if country \
-                                 not in creators_infos[creator][d_of_acc]:
-                                    creators_infos[creator][d_of_acc]. \
-                                        append(country)
+for year in node_dict:
+    for creator in node_dict[year]:
+        if type(node_dict[year][creator]) != str:
+            node_dict[year][creator] = mode(node_dict[year][creator])[0][0]
 
-                id_and_label = None
-                acronym = None if r.find('.//acronym') is None else \
-                    r.find('.//acronym').text
+for year in sorted(list(node_dict.keys())):
+    if not os.path.isdir(sys.argv[2] + framework + '/' + year):
+        os.mkdir(sys.argv[2] + framework + '/' + year)
 
-                context = r.find(".//context[@type = 'community']")
+    file_name = sys.argv[2] + framework + '/' + year + '/node_list.jsonl'
 
-                if context is not None:
-                    context = context.getchildren()
-
-                    if len(context) != 0:
-                        id_and_label = dict(context[0].getchildren()[-1].
-                                            attrib)
-
-                infos = [project_title]
-
-                if id_and_label is not None and acronym is not None:
-                    if id_and_label['id'] not in projects_table.keys():
-                        projects_table[id_and_label['id']] = {
-                            'label': id_and_label['label'], 'acronym': acronym}
-
-                    infos.append(id_and_label['id'])
-
-# STORING INFOS FOR THE NETWORK'S EDGES #######################################
-
-                for idx in np.arange(len(creators) - 1):
-                    cowork = [creators[i] for i in np.arange(idx + 1,
-                                                             len(creators))]
-                    for c in cowork:
-                        c1, c2 = None, None
-
-                        if creators[idx] < c:
-                            c1, c2 = creators[idx], c
-                        else:
-                            c1, c2 = c, creators[idx]
-
-                        if c1 not in adjacency_d.keys():
-                            adjacency_d[c1] = {
-                                c2: {d_of_acc: [infos]}}
-                        else:
-                            if c2 not in adjacency_d[c1].keys():
-                                adjacency_d[c1][c2] = {d_of_acc: [infos]}
-                            else:
-                                if d_of_acc not in adjacency_d[c1][c2].keys():
-                                    adjacency_d[c1][c2][d_of_acc] = [infos]
-                                else:
-                                    adjacency_d[c1][c2][d_of_acc].append(infos)
-
-framework = sys.argv[1].split('/')[-1].split('_')[1]
-
-# WRITING THE NETWORK'S NODE LIST #############################################
-
-with open('./' + framework + '_node_list.jsonl', 'w') as node_list:
-    for i in tqdm(creators_infos.items(), desc='WRITING NODE LIST'):
-        to_write = dict()
-        to_write[i[0]] = i[1]
-
-        json.dump(to_write, node_list)
-        node_list.write('\n')
-
-# WRITING THE NETWORK'S EDGE LIST #############################################
-
-with open('./' + framework + '_edge_list.jsonl', 'w') as edge_list:
-    for i in tqdm(adjacency_d.items(), desc='WRITING EDGE LIST'):
-        to_write = dict()
-        to_write[i[0]] = i[1]
-
-        json.dump(to_write, edge_list)
-        edge_list.write('\n')
-
-# WRITING THE PROJECTS' TABLE #################################################
-
-if len(projects_table.keys()) != 0:
-    save_to = '../../datasets/zenodo/' + framework + '/zenodo_' + framework + \
-        '_project_table.jsonl'
-
-    with open(save_to, 'w') as table:
-        for i in tqdm(projects_table.items(), desc='WRITING PROJECTS TABLE'):
+    with open(file_name, 'w') as node_list:
+        for creator in tqdm(node_dict[year].items(),
+                            desc='YEAR {}: WRITING NODE LIST'.format(year)):
             to_write = dict()
-            to_write[i[0]] = i[1]
+            to_write[creator[0]] = creator[1]
 
-            json.dump(to_write, table)
-            table.write('\n')
+            json.dump(to_write, node_list)
+            node_list.write('\n')
+
+for year in sorted(list(adjacency_d.keys())):
+    file_name = sys.argv[2] + framework + '/' + year + '/edge_list.jsonl'
+
+    with open(file_name, 'w') as edge_list:
+        for edge in tqdm(adjacency_d[year].items(),
+                         desc='YEAR {}: WRITING EDGE LIST'.format(year)):
+            to_write = dict()
+            to_write[edge[0]] = edge[1]
+
+            json.dump(to_write, edge_list)
+            edge_list.write('\n')
