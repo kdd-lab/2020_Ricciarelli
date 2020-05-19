fedecfa32d25abfeede26b9a6f5622181b37f62c
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-05-19 10:37:41 +0200

    Init entropies analysis script

---
+++ code/MAG_networks/entropies_analysis.py
@@ -0,0 +1,22 @@
+import json
+import numpy as np
+import sys
+
+from tqdm import tqdm
+
+entropies_matrix = list()
+
+with open(sys.argv[1], 'r') as entropies_file:
+    for row in tqdm(entropies_file, desc='READING ENTROPIES FILE'):
+        creator = list(json.loads(row.strip()).items())[0]
+
+        entropies_row = list()
+
+        for year in np.arange(1980, 2010):
+            if str(year) in creator[1]:
+                entropies_row.append(creator[1][str(year)])
+            else:
+                entropies_row.append(np.nan)
+
+        entropies_matrix.append(entropies_row)
+
