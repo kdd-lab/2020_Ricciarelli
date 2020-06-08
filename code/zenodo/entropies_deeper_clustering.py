9bb00ca5c32b97a231898487ff4ec3fde841d959
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-06-08 10:42:21 +0200

    Updated script

--- code/zenodo/entropies_deeper_clustering.py
+++ code/zenodo/entropies_deeper_clustering.py
@@ -9,18 +9,16 @@ from sklearn.metrics import silhouette_score
 from tqdm import tqdm
 
 log_filename = './logs/entropies_deeper_clustering_grid_search.log' \
-    if len(sys.argv == 5) else './logs/entropies_deeper_clustering_results.log'
+    if len(sys.argv) == 5 else './logs/entropies_deeper_clustering_results.log'
 
 logging.basicConfig(filename=log_filename, filemode='w', level=logging.INFO,
                     format='%(message)s')
 
-clustering_dataframe = pd.read_csv(sys.argv[2], dtype={'MAG_id': str,
-                                   'cluster': int})
-clustering_dataframe = \
-    clustering_dataframe[clustering_dataframe.cluster != int(sys.argv[3])]
+clustering_df = pd.read_csv(sys.argv[2], dtype={'MAG_id': str, 'cluster': int})
+valid_clustering_df = clustering_df[clustering_df.cluster != int(sys.argv[3])]
 
 entropies_dict, total_years = dict(), set()
-valid_MAG_ids = sorted(clustering_dataframe['MAG_id'].values.tolist())
+valid_MAG_ids = sorted(valid_clustering_df['MAG_id'].values.tolist())
 
 with open(sys.argv[1], 'r') as entropies_file:
     for row in tqdm(entropies_file, desc='READING ENTROPIES FILE'):
@@ -62,7 +60,7 @@ for i, row in tqdm(enumerate(entropies_matrix), desc='PREPROCESSING',
         if val == 1:
             entropies_matrix[i, j] = means[j]
 
-if len(sys.argv == 5):
+if len(sys.argv) == 5:
     for iteration in tqdm(np.arange(0, int(sys.argv[4])),
                           desc='GRID SEARCH ITERATION'):
         for clusters_number in tqdm(np.arange(2, 11), desc='GRID SEARCH'):
@@ -77,15 +75,17 @@ if len(sys.argv == 5):
 else:
     classifier = KMeans(n_clusters=2, random_state=42)
     labels = classifier.fit_predict(entropies_matrix)
+    labels = [1 for label in labels if label == 0 else 2]
     centroids = classifier.cluster_centers_
 
     clusters_infos, dataframe_infos = dict(), [[], [], []]
-    clusters_records = [[], [], [], [], []]
-    clusters_records_without_mean = [[], [], [], [], []]
+    clusters_records, clusters_records_without_mean = [[], []], [[], []]
 
-    for idx, MAG_id in tqdm(enumerate(sorted(entropies_dict)),
+    for idx, MAG_id in tqdm(enumerate(valid_MAG_ids),
                             desc='ASSIGNING CLUSTERS',
                             total=len(entropies_dict)):
+        clustering_df[clustering_df.MAG_id == MAG_id]['cluster'] = labels[idx]
+
         if labels[idx] not in clusters_infos:
             clusters_infos[labels[idx]] = \
                 {'years': set(list(entropies_dict[MAG_id])),
@@ -103,8 +103,50 @@ else:
             np.linalg.norm(entropies_matrix[labels[idx]] -
                            centroids[labels[idx]]))
 
-        clusters_records[labels[idx]].append(np.mean(entropies_matrix[idx]))
+        clusters_records[labels[idx] - 1].append(
+            np.mean(entropies_matrix[idx]))
 
-    clustering_dataframe = pd.DataFrame({'MAG_id': dataframe_infos[0],
-                                         'cluster': dataframe_infos[1],
-                                         'DFC': dataframe_infos[2]})
+    new_clustering_df = pd.DataFrame({'MAG_id': dataframe_infos[0],
+                                      'cluster': dataframe_infos[1],
+                                      'DFC': dataframe_infos[2]})
+
+    representative_records = list()
+
+    for cluster in sorted(new_clustering_df.cluster.unique()):
+        cluster_dataframe = \
+            new_clustering_df[new_clustering_df.cluster == cluster]
+
+        records = new_clustering_df\
+            .query('cluster == {} and DFC == {}'
+                   .format(cluster, min(cluster_dataframe['DFC']))).iloc[0:5]
+
+        for record in records.values:
+            representative_records\
+                .append({record[0]: entropies_dict[record[0]]})
+
+        for MAG_id in cluster_dataframe['MAG_id']:
+            entropies = list()
+
+            for year in entropies_dict[MAG_id]:
+                entropies.append(entropies_dict[MAG_id][year]['entropy'])
+
+            clusters_records_without_mean[cluster].append(np.mean(entropies))
+
+    creators_per_cluster = Counter(labels)
+
+    for cluster in sorted(creators_per_cluster):
+        percentage = \
+            np.round((creators_per_cluster[cluster] /
+                      len(valid_MAG_ids)) * 100, 2)
+
+        logging.info('CLUSTER {}:\n\t{} RESEARCHERS, {}% OF THE TOTAL\n'
+                     .format(cluster, creators_per_cluster[cluster],
+                             percentage))
+        logging.info('\tYEARS: {}\n'
+                     .format(sorted(clusters_infos[cluster]['years'])))
+        logging.info('\tCOUNTRIES: {}\n'
+                     .format(sorted(clusters_infos[cluster]['countries'])))
+        logging.info('\tREPRESENTATIVE RECORDS:\n')
+        for record in representative_records[int(cluster) * 5:
+                                             (int(cluster) * 5) + 5]:
+            logging.info('\t\t{}'.format(record))
