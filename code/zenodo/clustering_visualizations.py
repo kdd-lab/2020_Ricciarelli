233d13cf24c8a9d8d2fb0f0b99a8d3498c5df8b3
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-06-09 10:05:08 +0200

    Added boxplot plotting functionality

--- code/zenodo/clustering_visualizations.py
+++ code/zenodo/clustering_visualizations.py
@@ -1,8 +1,11 @@
+import json
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import sys
 
 from collections import defaultdict
+from tqdm import tqdm
 
 if sys.argv[1] == 'lineplot':
     f = open(sys.argv[2], 'r').readlines()
@@ -40,4 +43,38 @@ if sys.argv[1] == 'lineplot':
 
     plt.close(fig)
 elif sys.argv[1] == 'boxplot':
-    pass
+    entropies_dict = dict()
+
+    with open(sys.argv[2], 'r') as entropies_file:
+        for row in tqdm(entropies_file, desc='READING ENTROPIES FILE'):
+            creator = json.loads(row)
+
+            entropies_dict.update(creator)
+
+    cl_df = pd.read_csv(sys.argv[3])
+
+    entropies_per_cluster = [[] for cluster in cl_df.cluster.unique()]
+
+    for cluster in sorted(cl_df.cluster.unique()):
+        for MAG_id in cl_df[cl_df.cluster == cluster]['MAG_id']:
+            entropies = list()
+
+            for year in entropies_dict[MAG_id]:
+                entropies.append(entropies_dict[MAG_id][year]['entropy'])
+
+            entropies_per_cluster[cluster].append(np.mean(entropies))
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.boxplot(entropies_per_cluster,
+               labels=[str(c) for c in sorted(cl_df.cluster.unique())],
+               showfliers=False, showmeans=True)
+    ax.grid(linestyle='--', color='black', alpha=0.4)
+    ax.set_title("Entropies' Distribution per Clusteron a Deeper Level"
+                 if 'deeper' in sys.argv[3] else
+                 "Entropies' Distribution per Cluster", fontsize=20)
+    ax.set_xlabel('Cluster', fontsize=14)
+    ax.set_ylabel('Silhouette Score', fontsize=14)
+
+    fig.savefig('./images/entropies_distribution_deeper.pdf' if 'deeper'
+                in sys.argv[3] else './images/entropies_distribution.pdf',
+                format='pdf', bbox_inches='tight')
