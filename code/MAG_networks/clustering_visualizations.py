ef226add347bccfabc69d207c81f900d3fd1945d
germz01 <gianmarcoricciarelli@gmail.com>
2020-07-22 12:06:11 +0200

    Changed geoplots

--- code/MAG_networks/clustering_visualizations.py
+++ code/MAG_networks/clustering_visualizations.py
@@ -198,8 +198,8 @@ else:
             fig.colorbar(plt.cm.ScalarMappable(cmap='GnBu',
                                                norm=plt.Normalize(vmin=-1.0,
                                                                   vmax=1.0)),
-                         ax=ax[:, :], shrink=0.7, label='Entropy',
-                         orientation='horizontal')
+                         ax=ax[1, :], shrink=0.7, label='Entropy',
+                         location='bottom')
             fig.tight_layout(pad=2.0)
             fig.savefig(save_n, format='pdf', bbox_inches='tight')
 
