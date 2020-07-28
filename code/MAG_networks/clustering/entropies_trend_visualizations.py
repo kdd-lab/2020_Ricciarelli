2233cc8f3a88b6e5c6dcd5cfbb7754d9487cd852
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-07-28 17:28:27 +0200

    Update

--- code/MAG_networks/clustering/entropies_trend_visualizations.py
+++ code/MAG_networks/clustering/entropies_trend_visualizations.py
@@ -17,45 +17,73 @@ with open(sys.argv[1], 'r') as entropies_file:
 
 cl_df = pd.read_csv(sys.argv[2], dtype={'MAG_id': str, 'cluster': int})
 
+entropies_per_country, mean_entropies_per_country = dict(), dict()
+
+for MAG_id in cl_df[cl_df.cluster.isin([1, 2])]['MAG_id']:
+    for year in entropies_dict[MAG_id]:
+        country = entropies_dict[MAG_id][year]['affiliation']
+        entropy = entropies_dict[MAG_id][year]['entropy']
+
+        if country == 'United States':
+            country = 'United States of America'
+        elif country == 'Korea':
+            country = 'South Korea'
+        elif country == 'Russian Federation':
+            country = 'Russia'
+        elif country == 'Dominican Republic':
+            country = 'Dominican Rep.'
+        elif country == 'Bosnia and Herzegovina':
+            country = 'Bosnia and Herz.'
+        elif country == "Lao People's Democratic Republic":
+            country = 'Laos'
+        elif country == 'Cyprus':
+            country = 'N. Cyprus'
+        elif country == 'Central African Republic':
+            country = 'Central African Rep.'
+        elif country == 'South Sudan':
+            country = 'S. Sudan'
+        elif country == 'Syrian Arab Republic':
+            country = 'Syria'
+        elif country == 'Viet Nam':
+            country = 'Vietnam'
+
+        if country not in entropies_per_country:
+            entropies_per_country[country] = defaultdict(list)
+
+        entropies_per_country[country][year].append(entropy)
+
+entropies = dict()
+
+for country in ['Italy', 'Germany', 'France', 'Spain', 'United Kingdom',
+                'United States of America', 'Russia', 'China']:
+    entropies[country] = [np.mean(entropies_per_country[country][year])
+                          for year in sorted(entropies_per_country[country])]
+
+fig, ax = plt.subplots(1, 1, constrained_layout=True)
+
+for country in entropies:
+    ax.plot(np.arange(1980, 2020), entropies[country], linewidth=2,
+            label=country, alpha=0.5)
+
+ax.set_xlim(1979, 2020)
+ax.set_ylim(-1.0, 1.0)
+ax.set_xticks(np.arange(1980, 2020, 10))
+ax.set_xticks(np.arange(1980, 2020), minor=True)
+ax.tick_params(axis='both', which='major', labelsize=6)
+ax.set_title('Xenofilia/Xenophobia over the Years for various Countries',
+             fontsize=10)
+ax.set_xlabel('Year', fontsize=8)
+ax.set_ylabel('Xenofilia/Xenophobia', fontsize=8)
+ax.legend(loc='center left', fontsize=8, bbox_to_anchor=(1, 0.5))
+
+fig.savefig('entropies_trends_of_various_countries.pdf', format='pdf')
+
+plt.close(fig)
+
 for cluster in [1, 2]:
-    entropies_per_country, mean_entropies_per_country = dict(), dict()
-
-    for MAG_id in cl_df[cl_df.cluster == cluster]['MAG_id']:
-        for year in entropies_dict[MAG_id]:
-            country = entropies_dict[MAG_id][year]['affiliation']
-            entropy = entropies_dict[MAG_id][year]['entropy']
-
-            if country == 'United States':
-                country = 'United States of America'
-            elif country == 'Korea':
-                country = 'South Korea'
-            elif country == 'Russian Federation':
-                country = 'Russia'
-            elif country == 'Dominican Republic':
-                country = 'Dominican Rep.'
-            elif country == 'Bosnia and Herzegovina':
-                country = 'Bosnia and Herz.'
-            elif country == "Lao People's Democratic Republic":
-                country = 'Laos'
-            elif country == 'Cyprus':
-                country = 'N. Cyprus'
-            elif country == 'Central African Republic':
-                country = 'Central African Rep.'
-            elif country == 'South Sudan':
-                country = 'S. Sudan'
-            elif country == 'Syrian Arab Republic':
-                country = 'Syria'
-            elif country == 'Viet Nam':
-                country = 'Vietnam'
-
-            if country not in entropies_per_country:
-                entropies_per_country[country] = defaultdict(list)
-
-            entropies_per_country[country][year].append(entropy)
-
     for country in entropies_per_country:
         entropies = [entropies_per_country[country][year] for year
-                     in entropies_per_country[country]]
+                     in sorted(entropies_per_country[country])]
 
         if len(entropies) == 40:
             mean_entropies_per_country[np.mean(np.concatenate(entropies))] = \
@@ -73,8 +101,8 @@ for cluster in [1, 2]:
     fig, axes = plt.subplots(nrows=5, ncols=1, sharex=True,
                              constrained_layout=True)
 
-    fig.suptitle('Top 5 Countries with higher/lower Entropy - Cluster {}'
-                 .format(cluster), fontsize=10)
+    fig.suptitle('Top 5 Countries with higher/lower Xenofilia/Xenophobia - '
+                 'Cluster {}'.format(cluster), fontsize=10)
 
     for x in np.arange(0, 5):
         for l in [top_5_higher_entropies, top_5_lower_entropies]:
@@ -113,9 +141,8 @@ for cluster in [1, 2]:
         axes[x].set_xticks(np.arange(1980, 2020, 10))
         axes[x].set_xticks(np.arange(1980, 2020), minor=True)
         axes[x].tick_params(axis='both', which='major', labelsize=6)
-        # axes[x].set_title(l[x], fontsize=8)
         axes[x].set_xlabel('Year', fontsize=8)
-        axes[x].set_ylabel('Entropy', fontsize=8)
+        axes[x].set_ylabel('Xenofilia/Xenophobia', fontsize=8)
         axes[x].legend(loc='center left', fontsize=8,
                        bbox_to_anchor=(1, 0.5))
 
