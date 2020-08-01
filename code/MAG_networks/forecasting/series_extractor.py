c5bfedea2e1d2b6245c3aef517d9c32c04659924
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-08-01 16:29:23 +0200

    Updated extractor script

--- code/MAG_networks/forecasting/series_extractor.py
+++ code/MAG_networks/forecasting/series_extractor.py
@@ -15,18 +15,42 @@ with open(sys.argv[1], 'r') as entropies_file:
 
         entropies_dict.update(creator)
 
-valid_creators = dict()
+more_than_20_ents = dict()
 
-for mag_id in tqdm(valid_mag_ids, desc='SELECTING VALID CREATORS'):
+for mag_id in tqdm(valid_mag_ids,
+                   desc='SELECTING SERIES WITH MORE THAN 20 ENTRIES'):
     creator = entropies_dict[mag_id]
 
     if len(creator) >= 20:
-        valid_creators.update({mag_id: creator})
+        more_than_20_ents.update({mag_id: creator})
 
-saving_to = sys.argv[1].replace('global_entropies.jsonl',
-                                'valid_creators_time_series.jsonl')
+saving_to = sys.argv[1]\
+    .replace('/entropies/global_entropies.jsonl',
+             '/forecasting/series_with_more_than_20_entries.jsonl')
 
-with open(saving_to, 'w') as valid_creators_file:
-    for mag_id in tqdm(valid_creators, desc='SAVING VALID CREATORS'):
-        json.dump({mag_id: valid_creators[mag_id]}, valid_creators_file)
-        valid_creators_file.write('\n')
+with open(saving_to, 'w') as more_than_20_ents_file:
+    for mag_id in tqdm(more_than_20_ents,
+                       desc='SAVING SERIES WITH MORE THAN 20 ENTRIES'):
+        json.dump({mag_id: more_than_20_ents[mag_id]}, more_than_20_ents_file)
+        more_than_20_ents_file.write('\n')
+
+more_than_1_affiliation = dict()
+
+for mag_id in tqdm(more_than_20_ents, desc=''):
+    creator, countries = entropies_dict[mag_id], set()
+
+    for year in creator:
+        countries.add(creator[year]['affiliation'])
+
+    if len(countries) != 1:
+        more_than_1_affiliation.update({mag_id: creator})
+
+saving_to = saving_to.replace('series_with_more_than_20_entries.jsonl',
+                              'series_with_more_than_1_affiliation.jsonl')
+
+with open(saving_to, 'w') as more_than_1_affiliation_file:
+    for mag_id in tqdm(more_than_1_affiliation,
+                       desc='SAVING SERIES WITH MORE THAN 1 AFFILIATION'):
+        json.dump({mag_id: more_than_1_affiliation[mag_id]},
+                  more_than_1_affiliation)
+        more_than_1_affiliation.write('\n')
