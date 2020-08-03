ea065f2054b2a7696b0d7227f484b0a8a8d6eaab
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-08-03 10:57:49 +0200

    Updated forecastng script

--- code/MAG_networks/forecasting/series_forecasting.py
+++ code/MAG_networks/forecasting/series_forecasting.py
@@ -4,9 +4,7 @@ import sys
 import warnings
 
 from pmdarima.arima import AutoARIMA
-from sklearn.metrics import mean_squared_error
-from statsmodels.tsa.api import kpss
-from statsmodels.tsa.arima_model import ARIMA
+from pmdarima.model_selection import cross_val_score, RollingForecastCV
 from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
@@ -29,27 +27,14 @@ for mag_id in tqdm(time_series_raws, desc='PROCESSING THE SERIES'):
 
     series = np.array(series)
 
-    kpss_stat, p_value, lags, critical_values = kpss(series, nlags='auto')
-    is_stationary = abs(kpss_stat) < abs(critical_values['5%'])
+    model = AutoARIMA(start_p=0, d=None, start_q=0, max_p=5, max_d=5,
+                      max_q=5, m=1, seasonal=False, stationary=False,
+                      information_criterion='aic', alpha=0.05, test='kpss',
+                      stepwise=True, suppress_warnings=True,
+                      error_action='ignore')
+    cv = RollingForecastCV()
 
-    if not is_stationary:
-        print('NOT STATIONARY')
+    cv_score = cross_val_score(model, series, scoring='mean_squared_error',
+                               cv=cv)
 
-    split = int(len(series) * 0.66)
-    train_series, test_series = series[0:split], series[split:len(series)]
-    history, predictions = [sample for sample in train_series], list()
-
-    for time_step in range(len(test_series)):
-        model = AutoARIMA(start_p=0, d=None, start_q=0, max_p=5, max_d=5,
-                          max_q=5, m=1, seasonal=False, test='kpss',
-                          suppress_warnings=True, error_action='ignore')
-        model.fit(history, disp=0)
-
-        next_step = model.predict(1)
-
-        predictions.append(next_step[0])
-        history.append(test_series[time_step])
-
-    mse.append(mean_squared_error(test_series, predictions))
-
-print(np.mean(mse), np.std(mse))
+    mse.append(np.mean(cv_score))
