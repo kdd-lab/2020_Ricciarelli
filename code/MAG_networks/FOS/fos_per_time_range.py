c3af38032b380eb87d0783b8b1e47ef5f2d5ef04
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-08-10 10:41:47 +0200

    Updated fos extractor script

--- code/MAG_networks/FOS/fos_per_time_range.py
+++ code/MAG_networks/FOS/fos_per_time_range.py
@@ -3,6 +3,15 @@ import sys
 
 from tqdm import tqdm
 
+valid_mag_ids = set()
+
+with open(sys.argv[2], 'r') as entropies_file:
+    for row in tqdm(entropies_file, desc='READING ENTROPIES FILE'):
+        creator = json.loads(row)
+
+        for mag_id in creator:
+            valid_mag_ids.add(mag_id)
+
 fos_dict = dict()
 
 with open(sys.argv[1], 'r') as fos_file:
@@ -12,13 +21,14 @@ with open(sys.argv[1], 'r') as fos_file:
         if int(r[1]) >= 1980 and int(r[1]) <= 2019:
             fields = r[2].split('-')
 
-            if r[0] not in fos_dict:
-                fos_dict[r[0]] = {r[1]: fields}
-            else:
-                if r[1] not in fos_dict[r[0]]:
-                    fos_dict[r[0]][r[1]] = fields
+            if r[0] in valid_mag_ids:
+                if r[0] not in fos_dict:
+                    fos_dict[r[0]] = {r[1]: fields}
                 else:
-                    fos_dict[r[0]][r[1]] += fields
+                    if r[1] not in fos_dict[r[0]]:
+                        fos_dict[r[0]][r[1]] = fields
+                    else:
+                        fos_dict[r[0]][r[1]] += fields
 
 with open('./fields_of_study_1980_2019.jsonl', 'w') as fos_file_jsonl:
     for creator in tqdm(fos_dict.items(), desc='WRITING FIELDS OF STUDY'):
