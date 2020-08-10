1870cd61678ef3c3047a480845e58e5d3d233738
germz01 <gianmarcoricciarelli@gmail.com>
2020-08-10 12:13:52 +0200

    Added most represented fos per year image

--- code/MAG_networks/FOS/fos_visualizations.py
+++ code/MAG_networks/FOS/fos_visualizations.py
@@ -29,8 +29,8 @@ ax.plot(np.arange(1980, 2020),
 
 for idx, x in enumerate(np.arange(1980, 2020)):
     field_of_study = fos_counter_per_year[str(x)].most_common()[0][0]
-    y = ax.get_children()[0].properties()['data'][1][idx] + 100000
-    ax.text(x, y, field_of_study, fontdict={'fontsize': 4, 'rotation': 90,
+    y = ax.get_children()[0].properties()['data'][1][idx] + 50000
+    ax.text(x, y, field_of_study, fontdict={'fontsize': 4, 'rotation': 45,
             'ha': 'center', 'va': 'center'})
 
 ax.set_xlim(1979, 2020)
