fa7f376623ee79b909325562dee733002eec9ceb
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-08-23 16:08:24 +0200

    Added network statistics script

--- code/MAG_networks/networks_statistics.py
+++ code/MAG_networks/networks_statistics.py
@@ -1,3 +1,5 @@
+import gzip
+import igraph as ig
 import json
 import numpy as np
 import sys
@@ -8,7 +10,9 @@ decade = np.arange(int(sys.argv[1]), int(sys.argv[1]) + 10)
 
 authors_affiliations = dict()
 
-with open(sys.argv[2], 'r') as affiliations:
+file_name = '~/mydata/MAG_networks/authors_affiliation.json'
+
+with open(file_name, 'r') as affiliations:
     for affiliation in tqdm(affiliations, desc='READING AFFILIATIONS FILE'):
         if len(json.loads(affiliation.strip())) != 0:
             a, to_delete = json.loads(affiliation.strip()), list()
@@ -27,11 +31,28 @@ with open(sys.argv[2], 'r') as affiliations:
 
 affiliations_countries = dict()
 
-with open(sys.argv[3], 'r') as affiliations_countries_file:
+file_name = '~/mydata/MAG_networks/affiliations_geo.txt'
+
+with open(file_name, 'r') as affiliations_countries_file:
     for affiliation in tqdm(affiliations_countries_file,
                             desc='READING AFFILIATION COUNTRIES FILE'):
         a = affiliation.strip().split('\t')
 
         affiliations_countries.update(a)
 
-print(len(authors_affiliations), len(affiliations_countries))
+for year in decade:
+    g = ig.Graph()
+
+    file_name = '~/mydata/MAG_networks/{}/{}.gz'.format(year, year)
+
+    nodes_list, edges_list, weights_list = set(), list(), list()
+
+    with gzip.open(file_name, 'r') as es_list:
+        for edge in tqdm(es_list,
+                         desc='YEAR {}: READING EDGES LIST'.format(year)):
+            e = edge.decode().strip().split(',')
+
+            nodes_list.add(e[0])
+            nodes_list.add(e[1])
+            edges_list.append((e[0], e[1]))
+            weights_list.append(int(e[2]))
