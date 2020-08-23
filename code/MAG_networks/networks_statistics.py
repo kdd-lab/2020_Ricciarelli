8a866ead5bde74188e11c62bd0eedb94c4463185
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-08-23 12:33:22 +0200

    Added network statistics script

--- code/MAG_networks/networks_statistics.py
+++ code/MAG_networks/networks_statistics.py
@@ -8,27 +8,27 @@ decade = np.arange(int(sys.argv[1]), int(sys.argv[1]) + 10)
 
 authors_affiliations = dict()
 
-import ipdb
-ipdb.set_trace()
-
 with open(sys.argv[2], 'r') as affiliations:
     for affiliation in tqdm(affiliations, desc='READING AFFILIATIONS FILE'):
         if len(json.loads(affiliation.strip())) != 0:
-            a = json.loads(affiliation.strip())
+            a, to_delete = json.loads(affiliation.strip()), list()
 
             for mag_id in a:
                 for aff_id in a[mag_id]:
-                    if a[mag_id][aff_id]['from'] not in decade:
-                        del a[mag_id][aff_id]
+                    if len(a[mag_id][aff_id]) > 2:
+                        print(a)
+                    break
+#                     if a[mag_id][aff_id]['from'] not in decade:
+#                         del a[mag_id][aff_id]
 
-            if len(a[mag_id]) != 0:
-                authors_affiliations.update(a)
+#             if len(a[mag_id]) != 0:
+#                 authors_affiliations.update(a)
 
-affiliations_countries = dict()
+# affiliations_countries = dict()
 
-with open(sys.argv[3], 'r') as affiliations_countries_file:
-    for affiliation in tqdm(affiliations_countries_file,
-                            desc='READING AFFILIATION COUNTRIES FILE'):
-        a = affiliation.strip().split('\t')
+# with open(sys.argv[3], 'r') as affiliations_countries_file:
+#     for affiliation in tqdm(affiliations_countries_file,
+#                             desc='READING AFFILIATION COUNTRIES FILE'):
+#         a = affiliation.strip().split('\t')
 
-        affiliations_countries.update(a)
+#         affiliations_countries.update(a)
