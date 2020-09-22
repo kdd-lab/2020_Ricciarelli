e3b905aa44741f17dd117bb9948efd622b27713e
germz01 <gianmarcoricciarelli@gmail.com>
2020-09-22 12:46:48 +0200

    Added plot

--- code/MAG_networks/FOS/fos_general_visualizations.py
+++ code/MAG_networks/FOS/fos_general_visualizations.py
@@ -25,12 +25,14 @@ with open(sys.argv[1], 'r') as fos_file:
             for year in creator[mag_id]:
                 for fos in creator[mag_id][year]:
                     fos_counter[fos] += 1
-
+import ipdb
+ipdb.set_trace()
 fig, ax = plt.subplots(constrained_layout=True)
 ax.barh(np.arange(len(fos_counter)),
         [fos_counter[fos] for fos in sorted(fos_counter)], color='steelblue',
         edgecolor='steelblue', log=True)
 ax.set_title('Fields of Study in the Dataset')
+ax.set_yticks(np.arange(len(fos_counter)))
 ax.set_yticklabels([fos for fos in sorted(fos_counter)])
 fig.savefig('../images/fos/fos_in_the_dataset.pdf', bbox_inches='tight',
             format='pdf')
