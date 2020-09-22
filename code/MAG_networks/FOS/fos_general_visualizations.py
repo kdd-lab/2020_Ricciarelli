0536227472e80ff4ef70a590aa4dd28d464a4ea0
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-09-22 14:41:52 +0200

    Update script

--- code/MAG_networks/FOS/fos_general_visualizations.py
+++ code/MAG_networks/FOS/fos_general_visualizations.py
@@ -20,11 +20,15 @@ fos_counter = defaultdict(int)
 with open(sys.argv[1], 'r') as fos_file:
     for row in tqdm(fos_file, desc='READING FOS FILE'):
         creator = json.loads(row)
+        creator_fos = set()
 
         for mag_id in creator:
             for year in creator[mag_id]:
                 for fos in creator[mag_id][year]:
-                    fos_counter[fos] += 1
+                    creator_fos.add(fos)
+
+        for fos in creator_fos:
+            fos_counter[fos] += 1
 
 fig, ax = plt.subplots(constrained_layout=True)
 ax.barh(np.arange(len(fos_counter)),
@@ -32,7 +36,8 @@ ax.barh(np.arange(len(fos_counter)),
         edgecolor='steelblue', log=True)
 ax.set_title('Fields of Study in the Dataset')
 ax.set_yticks(np.arange(len(fos_counter)))
-ax.set_yticklabels([fos.replace('_', ' ').capitalize() for fos in sorted(fos_counter)])
+ax.set_yticklabels([fos.replace('_', ' ').capitalize()
+                   for fos in sorted(fos_counter)])
 fig.savefig('../images/fos/fos_in_the_dataset.pdf', bbox_inches='tight',
             format='pdf')
 plt.close(fig)
