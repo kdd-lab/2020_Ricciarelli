2f60e413096f068b74de62aa853dabb09492b5a0
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-09-22 14:52:16 +0200

    Update script

--- code/MAG_networks/FOS/fos_general_visualizations.py
+++ code/MAG_networks/FOS/fos_general_visualizations.py
@@ -15,7 +15,7 @@ matplotlib.rcParams['axes.labelsize'] = 10
 matplotlib.rcParams['xtick.labelsize'] = 8
 matplotlib.rcParams['ytick.labelsize'] = 8
 
-fos_counter = defaultdict(int)
+fos_counter, multidisciplinarity = defaultdict(int), defaultdict(int)
 
 with open(sys.argv[1], 'r') as fos_file:
     for row in tqdm(fos_file, desc='READING FOS FILE'):
@@ -30,6 +30,14 @@ with open(sys.argv[1], 'r') as fos_file:
         for fos in creator_fos:
             fos_counter[fos] += 1
 
+        multidisciplinarity[len(creator_fos)] += 1
+
+total_fos = sum([multidisciplinarity[key] for key in multidisciplinarity])
+
+for m in multidisciplinarity:
+    multidisciplinarity[m] = multidisciplinarity[m] / total_fos
+
+
 fig, ax = plt.subplots(constrained_layout=True)
 ax.barh(np.arange(len(fos_counter)),
         [fos_counter[fos] for fos in sorted(fos_counter)], color='steelblue',
@@ -39,5 +47,16 @@ ax.set_yticks(np.arange(len(fos_counter)))
 ax.set_yticklabels([fos.replace('_', ' ').capitalize()
                    for fos in sorted(fos_counter)])
 fig.savefig('../images/fos/fos_in_the_dataset.pdf', bbox_inches='tight',
+            format='pdf')
+plt.close(fig)
+
+fig, ax = plt.subplots(constrained_layout=True)
+ax.barh(np.arange(len(multidisciplinarity)),
+        [multidisciplinarity[k] for k in sorted(multidisciplinarity)],
+        color='steelblue', edgecolor='steelblue')
+ax.set_title('Multidisciplinarity in the Dataset')
+ax.set_yticks(np.arange(len(multidisciplinarity)))
+ax.set_yticklabels([multidisciplinarity[k] for k in sorted(fos_counter)])
+fig.savefig('../images/fos/multidisciplinarity.pdf', bbox_inches='tight',
             format='pdf')
 plt.close(fig)
